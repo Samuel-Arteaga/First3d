a47e4062f16beeb9362aa7e2e81e6170856e25d5
eric [itb] rodriguez luque <eric.rodriguez.7e6@itb.cat>
2024-03-07 16:47:37 +0000

    update

--- Assets/Scripts/Player/ActivateObject.cs
+++ Assets/Scripts/Player/ActivateObject.cs
@@ -8,18 +8,18 @@ public class ActivateObject : MonoBehaviour
     public GameObject bateMap;
     public GameObject batePlayer;
 
-    [Header("MASCARA DE SAMU")]
-    public GameObject mascaraMap;
-    public GameObject mascaraPlayer;
+    //[Header("MASCARA DE SAMU")]
+    //public GameObject mascaraMap;
+    //public GameObject mascaraPlayer;
 
-    [Header("CHALECO DE SAMU")]
-    public GameObject chalecoMap;
-    public GameObject chalecoPlayer;
+    [Header("CASCO DE SAMU")]
+    public GameObject cascoMap;
+    public GameObject cascoPlayer;
 
     void Start()
     {
         batePlayer.SetActive(false);
-        chalecoPlayer.SetActive(false);
+        cascoPlayer.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,15 +29,15 @@ public class ActivateObject : MonoBehaviour
             bateMap.SetActive(false);
             batePlayer.SetActive(true);
         }
-        else if (other.gameObject.CompareTag("mascara"))
+        //else if (other.gameObject.CompareTag("mascara"))
+        //{
+        //    mascaraMap.SetActive(false);
+        //    mascaraPlayer.SetActive(true);
+        //}
+        else if (other.gameObject.CompareTag("casco"))
         {
-            mascaraMap.SetActive(false);
-            mascaraPlayer.SetActive(true);
-        }
-        else if (other.gameObject.CompareTag("chaleco"))
-        {
-            chalecoMap.SetActive(false);
-            chalecoPlayer.SetActive(true);
+            cascoMap.SetActive(false);
+            cascoPlayer.SetActive(true);
         }
     }
 }
