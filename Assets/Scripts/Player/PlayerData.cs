ec59c649839f0dee80b86983a12c635d92ceb432
Samuel-Arteaga <samuel.arteaga.7e6@itb.cat>
2024-03-26 18:19:50 +0100

    Update Position JSON completed

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿
+[System.Serializable]
+public class PlayerData
+{
+    public float posX;
+    public float posY;
+    public float posZ;
+
+    public PlayerData(float x,float y, float z)
+    {
+        posX = x;
+        posY = y;
+        posZ = z;
+    }
+}
+
